c82aa6f965bc108ad6ce201f47d1d0100df2c938
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-07-11 11:59:42 +0100

    Socket IO added

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 import express from "express";
+//Socket IO:
+import { createServer } from "http";
+import { Server } from "socket.io";
+import connectionHandler from "./socket/index.js";
+//************** */
 import mongoose from "mongoose";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
@@ -10,9 +15,12 @@ import {
   unauthorizedHandler,
 } from "./errorHandlers.js";
 import userRouter from "./apis/users/index.js";
+import roomsRouter from "./apis/rooms/index.js";
 
 const server = express();
 const port = process.env.PORT || 3001;
+
+const httpServer = createServer(server);
 
 // ****************************************************** MIDDLEWARES **********************************************
 
@@ -23,6 +31,7 @@ server.use(express.json());
 // ******************************************************* ENDPOINTS ***********************************************
 
 server.use("/users", userRouter);
+server.use("/rooms", roomsRouter);
 
 // ***************************************************** ERROR HANDLERS ********************************************
 
@@ -32,12 +41,22 @@ server.use(unauthorizedHandler);
 server.use(forbiddenHandler);
 server.use(genericErrorHandler);
 
+const io = new Server(httpServer);
+io.on("connection", connectionHandler);
+
 mongoose.connect(process.env.MONGO_CONNECTION_URL);
 
-mongoose.connection.on("connected", () => {
+/* mongoose.connection.on("connected", () => {
   console.log("Successfully connected to Mongo!");
   server.listen(port, () => {
     console.table(listEndpoints(server));
     console.log(`Server is running on port ${port}`);
+  });
+}); */
+
+mongoose.connection.on("connected", () => {
+  httpServer.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server running on port ${port}`);
   });
 });
---
+++ src/utils/messages.js
@@ -0,0 +1,18 @@
+import MessagesModel from "../socket/models/messages.js";
+import RoomsModel from "../socket/models/rooms.js";
+
+export const saveMessage = async (message, roomName) => {
+  try {
+    const room = await RoomsModel.findOne({ name: roomName });
+    // save message in messages collection
+    const newMessage = new MessagesModel({
+      text: message.text,
+      sender: message.sender,
+      room: room._id,
+    });
+    const savedMessage = await newMessage.save();
+    return savedMessage;
+  } catch (error) {
+    console.log(error);
+  }
+};
