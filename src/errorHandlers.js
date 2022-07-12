2157ecef6cb38f94c55e350efdbcdeb24454866a
lbourke1234 <lbourke1234@gmail.com>
2022-07-12 08:48:40 +0100

    update error handlers

--- src/errorHandlers.js
+++ src/errorHandlers.js
@@ -1,35 +1,37 @@
+import mongoose from 'mongoose'
+
 export const badRequestHandler = (err, req, res, next) => {
   if (err.status === 400 || err instanceof mongoose.Error.ValidationError) {
-    res.status(400).send({ message: err.message });
+    res.status(400).send({ message: err.message })
   } else {
-    next(err);
+    next(err)
   }
-};
+}
 
 export const notFoundHandler = (err, req, res, next) => {
   if (err.status === 404) {
-    res.status(404).send({ message: err.message });
+    res.status(404).send({ message: err.message })
   } else {
-    next(err);
+    next(err)
   }
-};
+}
 export const unauthorizedHandler = (err, req, res, next) => {
   if (err.status === 401) {
-    res.status(401).send({ message: err.message });
+    res.status(401).send({ message: err.message })
   } else {
-    next(err);
+    next(err)
   }
-};
+}
 
 export const forbiddenHandler = (err, req, res, next) => {
   if (err.status === 403) {
-    res.status(403).send({ message: err.message });
+    res.status(403).send({ message: err.message })
   } else {
-    next(err);
+    next(err)
   }
-};
+}
 
 export const genericErrorHandler = (err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ message: "Generic Server Error" });
-};
+  console.log(err)
+  res.status(500).send({ message: 'Generic Server Error' })
+}
