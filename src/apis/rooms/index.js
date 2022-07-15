d060ee2dbfc7a406eef598d32de00071c576b9cb
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-07-15 15:22:19 +0100

    Update Redux store

--- src/apis/rooms/index.js
+++ src/apis/rooms/index.js
@@ -1,48 +1,59 @@
-import express from 'express'
-import createError from 'http-errors'
-import RoomsModel from '../rooms/model.js'
-import MessagesModel from '../messages/model.js'
+import express from "express";
+import createError from "http-errors";
+import RoomsModel from "../rooms/model.js";
+import MessagesModel from "../messages/model.js";
 /* import UsersModel from "../../models/users.js"; */
 
-const roomsRouter = express.Router()
+const roomsRouter = express.Router();
 
-roomsRouter.post('/', async (req, res, next) => {
+roomsRouter.post("/", async (req, res, next) => {
   try {
-    const newRoom = new RoomsModel(req.body)
-    const { _id } = await newRoom.save()
-    res.status(201).send({ _id })
+    const newRoom = new RoomsModel(req.body);
+    const { _id } = await newRoom.save();
+    res.status(201).send({ _id });
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
-roomsRouter.get('/:roomName/messages', async (req, res, next) => {
+roomsRouter.get("/", async (req, res, next) => {
   try {
-    const room = await RoomsModel.findOne({ name: req.params.roomName })
+    const rooms = await RoomsModel.find();
+
+    res.send(rooms);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+roomsRouter.get("/:roomName/messages", async (req, res, next) => {
+  try {
+    const room = await RoomsModel.findOne({ name: req.params.roomName });
     if (room) {
-      const messages = await MessagesModel.find({ room: room._id })
-      res.send(messages)
+      const messages = await MessagesModel.find({ room: room._id });
+      res.send(messages);
     } else {
-      next(createError(404, `Room with id ${req.params.roomName} not found!`))
+      next(createError(404, `Room with id ${req.params.roomName} not found!`));
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
-roomsRouter.get('/:roomId', async (req, res, next) => {
+});
+roomsRouter.get("/:roomId", async (req, res, next) => {
   try {
-    const foundRoom = await RoomsModel.findById(req.params.roomId)
+    const foundRoom = await RoomsModel.findById(req.params.roomId);
     if (!foundRoom) {
-      createError(404, 'room not found!')
+      createError(404, "room not found!");
     } else {
-      res.send(foundRoom)
+      res.send(foundRoom);
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
-export default roomsRouter
+export default roomsRouter;
